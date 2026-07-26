457de69ff0719aa6b0770c4435beec7dae1eddae
Simhadri Kanaka Raju <skanakaraju7674@gmail.com>
2026-07-26 08:20:38 +0530

    Refactor drowsiness check logic in alarm.py

--- src/alarm.py
+++ src/alarm.py
@@ -1,19 +1,13 @@
 import time
-
 drowsy_start_time = None
-
 
 def check_drowsiness(eye_status, yawn_status):
     global drowsy_start_time
-
     if eye_status == 1 or yawn_status == 1:
         if drowsy_start_time is None:
             drowsy_start_time = time.time()
-
         elif time.time() - drowsy_start_time >= 3:
             return True
-
     else:
         drowsy_start_time = None
-
     return False
